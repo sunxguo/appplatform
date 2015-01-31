f001ff142b9c0eadbe4f400aef3183b448dc4399
MonkeyKing <sunxguo@163.com>
2015-01-31 20:56:30 +0800

    admin

--- application/views/kmadmin/account.php
+++ application/views/kmadmin/account.php
@@ -2,19 +2,19 @@
 	<div class="titA">修改密码</div>
 	<form class="form-modify" action="/kmadmin/admin/modify" method="post" enctype="multipart/form-data">
 		<div class="tips-error w230" style="margin-left: 160px; width: 200px;" id="errorInfo"></div>
-		<div class="item">
+		<div class="item" style="padding-left: 60px;">
 			<input class="inp-txt width200" type="text" name="username" id="username" placeholder="用户名" value="<?php echo $_SESSION['username'];?>"/>
 			<span style="color:red;">*</span>
 		</div>
-		<div class="item">
+		<div class="item" style="padding-left: 60px;">
 			<input class="inp-txt width200" type="password" name="oldpwd" id="oldpwd" placeholder="旧密码"/>
 			<span style="color:red;">*</span>
 		</div>
-		<div class="item">
+		<div class="item" style="padding-left: 60px;">
 			<input class="inp-txt width200" type="password" name="newpwd" id="newpwd" placeholder="新密码"/>
 			<span style="color:red;">*</span>
 		</div>
-		<div class="item">
+		<div class="item" style="padding-left: 60px;">
 			<input class="inp-txt width200" type="password" name="renewpwd" id="renewpwd" placeholder="确认密码"/>
 			<span style="color:red;">*</span>
 		</div>
